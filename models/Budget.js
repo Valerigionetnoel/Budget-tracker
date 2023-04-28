a900a83eee6d77b612e8624911f55e34b92bf8fa
Valéri Gionet-Noël <valeri.gionetnoel@gmail.com>
2023-04-27 21:09:01 -0300

    add Budget class and Spending class

---
+++ models/Budget.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Budget extends Model {}
+
+Budget.init(
+    {
+        month: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        income: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        expenses: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+        savings: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'data',
+    }
+);
+
+module.exports = Budget
---
+++ renderData.js
@@ -0,0 +1,26 @@
+const Data = require('./models/Data')
+const Chart = require('chart.js');
+const canvas = document.getElementById('dataChartTest');
+const ctxx = canvas.getContext('2d');
+
+const data = Data.findAll({
+  attributes: ['purchase_name', 'cost']
+});
+
+const chartData = {
+  labels: data.map(d => d.purchase_name),
+  datasets: [
+    {
+      label: 'Values',
+      data: data.map(d => d.cost),
+      backgroundColor: 'rgba(255, 99, 132, 0.2)',
+      borderColor: 'rgba(255, 99, 132, 1)',
+      borderWidth: 1
+    }
+  ]
+};
+
+const myChart = new Chart(ctxx, {
+  type: 'pie',
+  data: chartData
+});
