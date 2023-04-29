46e53ac45e0791286765afaa8f161cdc8ed2c7dc
StunnaDawg <jonsonallen9@gmail.com>
2023-04-28 21:41:45 -0300

    add transaction modal complete

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,47 @@
 const newTransactionFormHandler = async (event) => {
+    event.preventDefault();
 
-}
+    const company = document.querySelector('#companyName').value.trim();
+    const amount = document.querySelector('#amountPurchase').value.trim();
+    const category = document.querySelector('#categoryOfPurchase').value.trim();
+    const notes = document.querySelector('#purchaseNote').value.trim();
+    const emotion = document.querySelector('#emotionalPurchase').value.trim();
+
+    if (company && amount && category && notes && emotion) {
+        const response = await fetch(`/api/data`, {
+            method: 'POST',
+            body: JSON.stringify ({ company, amount, category, notes, emotion }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        },
+)};
+
+        if (response.ok) {
+            document.location.replace('/data')
+        } else {
+            alert('Failed to create project');
+          }
+};    
+
+const delTransaction= async (event) => {
+    if (event.target.hasAttribute('data_id')) {
+      const id = event.target.getAttribute('data_id');
+  
+      const response = await fetch(`/api/data/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/data');
+      } else {
+        alert('Failed to delete project');
+      }
+    }
+  };
+
+  document.querySelector('#transactionForm')
+  .addEventListener('submit', newTransactionFormHandler);
+
+  document.querySelector('.transaction-list')
+  .addEventListener('click', delTransaction);
---
+++ public/js/modal.js
@@ -0,0 +1,13 @@
+
+
+
+
+const newTransactionModal = async (event) => {
+    event.preventDefault();
+
+    const showModal = document.querySelector('staticModal');
+    
+
+}
+
+document.querySelector('#addModal').addEventListener('click');
