03ec6e75d34998fc82aa5759677582b4d678110c
APOLAKl <122183428+APOLAKl@users.noreply.github.com>
2023-05-01 01:00:50 -0300

    Spend route is good but untested, please accept budget route first

--- controllers/api/budgetRoutes.js
+++ controllers/api/budgetRoutes.js
@@ -64,7 +64,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!budgetData) {
-      res.status(404).json({ message: 'No project found with this id!' });
+      res.status(404).json({ message: 'No budget data found with this id!' });
       return;
     }
 
